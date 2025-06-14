d7106ed706d9206244c434a799e6f0bb001b417f
Darshan-gowda-m <155784473+Darshan-gowda-m@users.noreply.github.com>
2025-06-14 17:11:13 +0530

    Delete backend/routes directory

--- backend/routes/mentorshipRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/mentorshipController')
-const auth = require('../middleware/auth')
-router.use(auth.authenticate)
-router.get('/active', controller.getActiveMentorships)
-module.exports = router
